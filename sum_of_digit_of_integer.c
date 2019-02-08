c422257c346cb91582a0ba53b042788ea277e141
SysHouse <aamirakbari@gmail.com>
2019-02-08 19:33:27 +0530

    Create sum_of_digit_of_integer.c

---
+++ sum_of_digit_of_integer.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+
+int main(void)
+{
+        int num;
+        printf("Enter the number : ");
+        scanf("%d",&num);
+
+        printf("\b = %d\n",sum_of_digit(num));
+        return 0;
+}
+
+int sum_of_digit(int num )
+{
+        int sum;
+
+        if(num / 10 == 0)
+        {
+//              printf("%d+",num);
+                return num;
+        }
+        sum = (num % 10 ) + sum_of_digit(num / 10);
+//      printf("%d+",num%10);
+        return sum;
+}
